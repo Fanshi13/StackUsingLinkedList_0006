af35ea00b4e56aa969959b907b5d75b67ba80e4a
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-05 11:15:02 +0700

    Validasi pilihan menu dengan pesan kesalahan

--- stack.cpp
+++ stack.cpp
@@ -101,5 +101,8 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
         }
     }
